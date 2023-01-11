48c390aaeef3051e4a486b91676662fd5401282d
Marcelo Amorim <marceloamorim.fa@gmail.com>
2023-01-11 09:27:52 +0000

    Install dependencies and create Employee e DataContext Models

---
+++ EmployeesCrud/EmployeesCrud/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeesCrud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesCrud.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
